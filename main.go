e9089a0e6aefdae8c728908c9201e4bab04ccf0f
Ayush0-8Biswas <biswas.ayush03@kgpian.iitkgp.ac.in>
2022-04-26 11:35:24 +0530

    changed bot token

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-	apiToken := "5238894566:AAEvB2BgSISLA_tl_Cs6bpDU2lR66JbkMmI"
+	apiToken := "5238894566:AAEprJ_XDOWzFBtVGa5qbZtQRw06Dmmn6C8"
 	baseUrl := "https://api.telegram.org/bot" + apiToken
 	botName := "@AB22TGBot"
 
